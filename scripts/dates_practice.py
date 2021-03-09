a2300c2dbecb5fa1bc70f0698ac0493e3e2df742
joan18 <joan@conchalabs.com>
2021-03-08 19:24:18 -0500

    dates practice script

---
+++ scripts/dates_practice.py
@@ -0,0 +1,44 @@
+from gtts import gTTS
+import utils
+from playsound import playsound
+import random
+import time
+import datetime
+
+
+def random_date():
+    start_date = datetime.date(1900, 1, 1)
+    end_date = datetime.date(2021, 4, 1)
+    
+    time_between_dates = end_date - start_date
+    days_between_dates = time_between_dates.days
+    random_number_of_days = random.randrange(days_between_dates)
+    random_date = start_date + datetime.timedelta(days=random_number_of_days)
+    return random_date
+
+
+def parsed_str_date():
+    date_str = random_date().strftime("%d %B, %Y")
+    if date_str[0] == '0':
+        date_str = date_str[1:]
+    return date_str
+
+
+def execute_script(practice_type=0, wait_time=3.0):
+    while True:
+        date_str = parsed_str_date()
+        file = utils.audio.generate_fr_audio(date_str)
+        if practice_type == 0:
+            playsound(f'../audios/{file}')
+            time.sleep(wait_time)
+            print(date_str)
+        elif practice_type == 1:
+            print(date_str)
+            time.sleep(wait_time)
+            playsound(f'../audios/{file}')
+        else:
+            break
+
+
+if __name__ == '__main__':
+    execute_script(practice_type=1, wait_time=10)
--- scripts/numbers_practice.py
+++ scripts/numbers_practice.py
@@ -10,24 +10,22 @@ def build_audio_files():
         utils.audio.generate_fr_audio(f'{i}', 'number-')
 
 
-def execute_script(from_number=0, to_number=100, practice_type=0):
+def execute_script(from_number=0, to_number=100, practice_type=0, wait_time=3.0):
     numbers = list(range(from_number, to_number + 1))
     while True:
         rand_number_id = random.randint(0, len(numbers))
         rand_number = numbers[rand_number_id]
         if practice_type == 0:
             playsound(f'../audios/number-{rand_number}.mp3')
-            time.sleep(3)
+            time.sleep(wait_time)
             print(f'Number is {rand_number}')
         elif practice_type == 1:
             print(f'Number {rand_number}')
-            time.sleep(3)
+            time.sleep(wait_time)
             playsound(f'../audios/number-{rand_number}.mp3')
         
         del numbers[rand_number_id]
 
 
 if __name__ == '__main__':
-    execute_script(practice_type=1)
-
-# TODO dates practice, randomly a date
+    execute_script(practice_type=1, wait_time=2.5)
